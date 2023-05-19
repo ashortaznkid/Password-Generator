0b7576946e03dc8ccec0e022f0476b3b66191a93
ashortaznkid <mccue.nguyen@gmail.com>
2023-05-18 18:13:23 -0700

    removed uneccessary files

--- script.js
+++ script.js
@@ -35,7 +35,43 @@ function questions() {
   return responses;
 }
 
-/*
+function generatePassword() {
+  var passwordOptions = questions();
+  var possibleCombo = [];
+  var finalPassword = "";
+
+
+
+  if (passwordOptions.askNumbers) {
+    for (var i of numbers)
+      possibleCombo.push(i);
+  }
+  if (passwordOptions.askLowerCase) {
+    for (var i of lowerCase)
+      possibleCombo.push(i);
+  }
+  if (passwordOptions.askUpperCase) {
+    for (var i of upperCase)
+      possibleCombo.push(i);
+  }
+  if (passwordOptions.askSpecial) {
+    for (var i of special)
+      possibleCombo.push(i);
+  }
+
+
+  console.log(possibleCombo);
+
+
+  for (var i = 0; i < passwordOptions.length; i++) {
+    finalPassword += possibleCombo[Math.floor(Math.random() * possibleCombo.length)];
+    
+  }
+  console.log(finalPassword);
+
+  return finalPassword;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
